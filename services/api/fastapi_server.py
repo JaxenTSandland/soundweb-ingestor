235be3ff233d493d53472a3adadd0e52b3875932
JaxenTSandland <jsandland@student.neumont.edu>
2025-04-28 19:52:32 -0600

    Added healthcheck

--- services/api/fastapi_server.py
+++ services/api/fastapi_server.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/api/test")
 def api_test():
     return {"success": True, "message": "Ingestor API is running."}
+
+@app.get("/api/health")
+def health_check():
+    return {"status": "ok"}
